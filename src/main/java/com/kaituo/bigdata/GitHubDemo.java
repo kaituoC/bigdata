51c99f1e6c9302e698883fe0dc12f8df6b96a3a0
kaituo <changktforever@163.com>
2017-03-23 23:29:43 +0800

    新建我的GitHub第一个java类

---
+++ src/main/java/com/kaituo/bigdata/GitHubDemo.java
@@ -0,0 +1,10 @@
+package com.kaituo.bigdata;
+
+/**
+ * Created by chang on 2017/3/23.
+ */
+public class GitHubDemo {
+    public static void main(String[] args) {
+        System.out.println("This is my first GitHub java project!");
+    }
+}
